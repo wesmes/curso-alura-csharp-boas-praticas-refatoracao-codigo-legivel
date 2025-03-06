3a20224538f79344b11f7b6afd92b689ac54c89d
Weslley Gomes da Cruz <wgomes@rtcti.com>
2025-03-06 11:23:56 -0300

    Módulo 1 - Aula 1: Apresentação

---
+++ Alura.Adopet.Console/Show.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console
+{
+    internal class Show
+    {
+        public void ExibeConteudoArquivo(string caminhoDoArquivoASerExibido)        {
+          
+            using (StreamReader sr = new StreamReader(caminhoDoArquivoASerExibido))
+            {
+                System.Console.WriteLine("----- Dados a serem importados -----");
+                while (!sr.EndOfStream)
+                {
+                    // separa linha usando ponto e vírgula
+                    string[]? propriedades = sr.ReadLine().Split(';');
+                    // cria objeto Pet a partir da separação
+                    Pet pet = new Pet(Guid.Parse(propriedades[0]),
+                    propriedades[1],
+                    int.Parse(propriedades[2]) == 1 ? TipoPet.Gato : TipoPet.Cachorro
+                    );
+                    System.Console.WriteLine(pet);
+                }
+            }
+        }
+    }
+}
