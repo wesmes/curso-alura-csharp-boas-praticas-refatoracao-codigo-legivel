3ee21210bd5793e33a17349e8f92378a59b6fd7f
Weslley Gomes da Cruz <w_gomes@live.com>
2025-10-06 17:02:03 -0300

    Módulo 3 - Aula 13: Classe ComandosDoSistema para encapsular a coleção de comandos do sistema

---
+++ Alura.Adopet.Console/Comandos/ComandosDoSistema.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Console.Comandos
+{
+    internal class ComandosDoSistema
+    {
+        private Dictionary<string, IComando> comandosDoSistema = new()
+        {
+            {"help",new Help() },
+            {"import",new Import() },
+            {"list",new List() },
+            {"show",new Show() },
+        };
+
+        public IComando? this[string key] => comandosDoSistema.ContainsKey(key) ? comandosDoSistema[key] : null;
+    }
+}
--- Alura.Adopet.Console/Program.cs
+++ Alura.Adopet.Console/Program.cs
@@ -3,27 +3,14 @@ using System.Net.Http.Json;
 using Alura.Adopet.Console.Comandos;
 using Alura.Adopet.Console.Modelos;
 
-Dictionary<string, IComando> comandosDoSistema = new()
-{
-    {"import", new Import() },
-    { "help", new Help() },
-    { "show", new Show() },
-    { "list", new List() }
-};
+ComandosDoSistema comandos = new();
 Console.ForegroundColor = ConsoleColor.Green;
 try
 {
     string comando = args[0].Trim();
-
-    if (comandosDoSistema.ContainsKey(comando))
-    {
-        IComando? cmd = comandosDoSistema[comando];
-        await cmd.ExecutarAsync(args);
-    }   
-    else
-    {
-        Console.WriteLine("Comando inválido! Utilize o comando 'adopet help' para ver a lista de comandos disponíveis.");
-    }
+    IComando? cmd = comandos[comando];
+    if (cmd is not null) await cmd.ExecutarAsync(args);
+    else Console.WriteLine($"Comando inválido");
 }
 catch (Exception ex)
 {
