8641439e34c99bbecc3a238b19cee221069fec0a
Weslley Gomes da Cruz <w_gomes@live.com>
2025-10-07 10:52:41 -0300

    Módulo 4 - Aula 9: Nova regatoração com testes

---
+++ Alura.Adopet.Testes/PetAPartirDoCSVTest.cs
@@ -0,0 +1,24 @@
+﻿using Alura.Adopet.Console.Modelos;
+using Alura.Adopet.Console.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Adopet.Testes
+{
+    public class PetAPartirDoCSVTest
+    {
+        [Fact]
+        public void QuandoUmaStringForValidaDeveRetornarUmPet()
+        {
+            //Arrange
+            string linha = "456b24f4-19e2-4423-845d-4a80e8854a41;Lima Limão;1";
+            //Act
+            Pet pet = linha.ConverteDoTexto();
+            //Assert
+            Assert.NotNull(pet);
+        }
+    }
+}
